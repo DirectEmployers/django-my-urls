de99a5209b7898a68e05462dbe182cbc5d972ee3
Mike Seidle <mike@directemploers.org>
2011-11-28 17:26:22 -0500

    Continued branding refactor

--- django-my-urls/example/settings.py
+++ django-my-urls/example/settings.py
@@ -124,6 +124,7 @@ MYURLS_CHARACTER_SET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRST
 # No vowel character set (prevents bad words in URLS)
 # MYURLS_CHARACTER_SET = "0123456789bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
 MYURLS_DEFAULT_REDIRECT_TYPE = '302'
+MYURLS_DEFAULT_SCHEME = 'http://'
 
 # Support for loading local settings for development
 
