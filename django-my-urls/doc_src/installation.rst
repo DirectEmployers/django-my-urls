47ba8e0f9a0dc3efefe6054e93bf9f87bca54d2f
Mike Seidle <mike@directemploers.org>
2011-11-28 10:00:14 -0500

    Add existing code and refactor new app name.

---
+++ django-my-urls/doc_src/installation.rst
@@ -0,0 +1,17 @@
+
+Installation
+============
+
+Installation is easy using ``pip`` or ``easy_install``.
+
+.. code-block:: bash
+
+	pip install django-my-urls
+
+or
+.. code-block:: bash
+
+	easy_install django-my-urls
+
+Dependencies
+************
---
+++ django-my-urls/myurls/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponseForbidden, Http404
+from django.contrib.models import User
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import DetailView, ListView
+from django.template import RequestContext
+from django.utils.translation import ugettext, ugettext_lazy as _
+
+from myurls.models import Shorty, Click
+
+
+class MyUrlsList(ListView):
+    """implements end user view of short url history"""
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ProtectedView, self).dispatch(*args.**kwargs)
+    
+class MyUrlsDetail(DetailView):
+    """implements end user detail view of short url"""
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ProtectedView, self).dispatch(*args.**kwargs)
+
+class EditURLDetail(For
+    """implements end user edit view for short url"""
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ProtectedView, self).dispatch(*args.**kwargs)
+
