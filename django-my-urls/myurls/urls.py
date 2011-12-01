d2ed712371a5d9c8ed333c3aead63d3805ba770b
Mike Seidle <mike@directemploers.org>
2011-12-01 10:41:25 -0500

    added make short URL from GET request view

--- django-my-urls/myurls/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from myurls.views import ListView
-
-urlpatterns = patterns('',
-    #url(r'/list',views.MyUrlsList.as_view()),
-    url(r'^$', 'views.index', name='index'),
-    )
