c7beb8598db8bee455162c0e012eabccc293831c
Mike Seidle <mike@directemploers.org>
2011-12-02 11:13:13 -0500

    fix middleware redirect

--- django-my-urls/myurls/middleware.py
+++ django-my-urls/myurls/middleware.py
@@ -1,7 +1,7 @@
 from django import http
 from django.conf import settings
 
-from myurls.models import MyUrl
+from myurls.models import MyUrl, Click
 
 class MyUrlsFallbackMiddleware(object):
     """Checks for short URL and redirects on 404s"""
@@ -15,32 +15,56 @@ class MyUrlsFallbackMiddleware(object):
         # get the myurl - even if someone appends a trailing /
         if path.endswith('/'):
             try:
-                myurl = MyUrl(site__id__exact=settings.SITE_ID,
-                              short_path=path[:path.rfind('/')]+path[path.rfind('/')+1:])
-            except MyUrl.DoesNotExist:
-                return response
+                myurl = MyUrl.objects.get(
+                    short_path__exact=path[:path.rfind('/')]+path[path.rfind('/')+1:])
+            except MyUrl.DoesNotExist:                
+                myurl=None
         else:
             try:
-                myurl = MyUrl.objects.get(short_url__exact=request.path)
+                myurl = MyUrl.objects.get(short_path__exact=path)
             except MyUrl.DoesNotExist:
-                return response
+                myurl=None
                 # need to put somethign here for failed clicks.
+        # see if myurl is defined
         # Makes sure we are not redirecting to nowhere
-        if myurl is not None:
-            if myurl.redirect_url == '':
+        if myurl is None:
                 return http.HttpResponseGone()
+        
         else:
             # create and save the click history
+            if request.user is not None:
+                user = request.user
+            # Store HTTP headers if we have them
+            try:
+                referrer_url = request.META.HTTP_REFERRER
+            except: KeyError:
+                referrer_url = None
+            try:
+                user_domain = request.META.REMOTE_HOST
+            except: KeyError:
+                user_domain = None
+            try:
+                user_language = request.META.HTTP_ACCEPT_LANGUAGE
+            except: KeyError:
+                user_language = None
+            try:
+                user_agent = request.META.HTTP_USER_AGENT
+            except:
+                user_agent = None
+            try:
+                user_ip = request.META.REMOTE_ADDR
+            except:
+                user_ip = None
             click = Click(myurl=myurl,
                           to_url=myurl.to_url,
                           redirect_url=myurl.redirect_url,
-                          referrer_domain=request.META.HTTP_REMOTE_HOST,
-                          referrer_url=request.META.HTTP_REFERRER,
+                          user_domain=user_domain,
+                          referrer_url=referrer_url,
                           site=Site.objects.get_current(),
-                          user=request.user,
-                          user_ip=request.META.HTTP_HOST,
-                          user_language=request.META.HTTP_ACCEPT_LANGUAGE,
-                          user_agent=request.META.HTTP_USER_AGENT)
+                          user=user,
+                          user_ip=user_ip,
+                          user_language=user_language,
+                          user_agent=user_agent)
             click.save()
             # do the redirect
             if myurl.redirect_type == '301':
